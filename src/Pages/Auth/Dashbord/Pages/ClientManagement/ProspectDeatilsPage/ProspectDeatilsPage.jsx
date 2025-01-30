59d29ea0ce5be058b4d04be890ab62fd2b5819ee
sleh <slehdine.bouchoucha@gmail.com>
2025-01-30 17:26:21 +0100

    fix the meeting section

--- src/Pages/Auth/Dashbord/Pages/ClientManagement/ProspectDeatilsPage/ProspectDeatilsPage.jsx
+++ src/Pages/Auth/Dashbord/Pages/ClientManagement/ProspectDeatilsPage/ProspectDeatilsPage.jsx
@@ -11,7 +11,7 @@ const ProspectDeatilsPage = () => {
     <div className=' flex flex-row items-center justify-center border-b-2 my-3 border-[#BC983E]'>
       <span className=' font-jakarta text-l w-32  font-bold size-6  text-[#BC983E]'>Historique </span>
     </div>
-    <div className='flex-1 w-full'>hahahahah</div>
+    <div className='flex-1 w-full'>meetings table</div>
     
     </div>
   )
