81a653f0f8f67ee7940f9ffdb0f9b6ac7a90c0c6
Montassar Sakouhi <Montassa015@gmail.com>
2025-06-19 16:59:18 +0100

    test

--- src/Pages/Auth/Dashbord/Home.jsx
+++ src/Pages/Auth/Dashbord/Home.jsx
@@ -1,13 +1,12 @@
-
-
 import React from 'react'
+
 const Home = () => {
-  const word ="hello from the dark side "
-  return (
-    <div className="h-full w-full">
-       </div>
-  
-  )
+    return (
+        <div className="h-full w-full">
+
+        </div>
+
+    )
 }
 
 export default Home
