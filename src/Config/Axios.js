145303b6b75e536e3ff1cea48bf28012e3095651
MontassarSakouhi <Montassa015@gmail.com>
2025-07-02 12:57:56 +0100

    test

--- src/Config/Axios.js
+++ src/Config/Axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 const Axios = axios.create({
-    baseURL: "https://serveur.leaders-building.com/api",
-    withCredentials: true,
+    baseURL: import.meta.env.VITE_API_URL || "https://serveur.leaders-building.com/api",
+    withCredentials: true, // Always enable credentials for authentication
     headers: {"  Content-Type": "application/json", "x-api-key": apiKey},
 });
 
